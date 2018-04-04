746e308e6616a90257e46be9f9fcaaee69ecfffe
Nemanja <ngacic30@gmail.com>
2018-04-04 10:44:00 +0200

    Add navigation drawer.

--- app/src/main/java/com/nemanja97/androidposts/ReadPostActivity.java
+++ app/src/main/java/com/nemanja97/androidposts/ReadPostActivity.java
@@ -1,16 +1,36 @@
 package com.nemanja97.androidposts;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.nemanja97.androidposts.adapters.DrawerListAdapter;
+import com.nemanja97.androidposts.model.NavItem;
+
+import java.util.ArrayList;
+
 public class ReadPostActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private RelativeLayout mDrawerPane;
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+    private ArrayList<NavItem> mNavItems = new ArrayList<NavItem>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +42,56 @@ public class ReadPostActivity extends AppCompatActivity {
         final android.support.v7.app.ActionBar actionBar = getSupportActionBar();
 
         if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
+           // actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeButtonEnabled(true);
         }
 
+        prepareMenu(mNavItems);
+
+        mTitle = mDrawerTitle = getTitle();
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mDrawerList = (ListView) findViewById(R.id.navList);
+
+        // Populate the Navigtion Drawer with options
+        mDrawerPane = (RelativeLayout) findViewById(R.id.drawerPane);
+        DrawerListAdapter adapter = new DrawerListAdapter(this, mNavItems);
+
+
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        mDrawerList.setAdapter(adapter);
+
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                toolbar,
+                R.string.drawer_open,
+                R.string.drawer_close
+        ) {
+            public void onDrawerClosed(View view) {
+//                getActionBar().setTitle(mTitle);
+                getSupportActionBar().setTitle(mTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+//                getActionBar().setTitle(mDrawerTitle);
+                getSupportActionBar().setTitle("AndroidPost");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+    }
+
+    private void prepareMenu(ArrayList<NavItem> mNavItems ){
+        mNavItems.add(new NavItem(getString(R.string.settings), "", R.drawable.ic_launcher_background));
+        mNavItems.add(new NavItem(getString(R.string.posts_label), "", R.drawable.ic_launcher_background));
+
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getSupportActionBar().setTitle(mTitle);
     }
 
     @Override
@@ -36,6 +102,32 @@ public class ReadPostActivity extends AppCompatActivity {
             startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItemFromDrawer(position);
+        }
+    }
+
+    private void selectItemFromDrawer(int position) {
+        if(position == 0){
+            Intent preference = new Intent(ReadPostActivity.this,SettingsActivity.class);
+            startActivity(preference);
+        }else if(position == 1){
+            Intent preference = new Intent(ReadPostActivity.this,PostsActivity.class);
+            startActivity(preference);
+        }else{
+            Log.e("DRAWER", "Nesto van opsega!");
+        }
+
+        mDrawerList.setItemChecked(position, true);
+        if(position != 1) // za sve osim za sync
+        {
+            setTitle(mNavItems.get(position).getmTitle());
+        }
+        mDrawerLayout.closeDrawer(mDrawerPane);
     }
 
     @Override
